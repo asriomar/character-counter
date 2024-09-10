4a9a77a2001a4b6daf73d3b2ec412472a0a614eb
Mohd Asri Omar <62752924+asriomar@users.noreply.github.com>
2024-09-10 15:43:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ function updateCounter() {
   remainingCounterEl.innerText =
     textareaEl.getAttribute("maxLength") - textareaEl.value.length;
 }
+
+// CHANGE TO THIS CODE IF YOU WANT WORD COUNTER
+
+// const textareaEl = document.getElementById("textarea");
+// const totalCounterEl = document.getElementById("total-counter");
+// const remainingCounterEl = document.getElementById("remaining-counter");
+
+// textareaEl.addEventListener("keyup", () => {
+//   updateCounter();
+// });
+
+// updateCounter();
+
+// function updateCounter() {
+//   const wordsArray = textareaEl.value.trim().split(/\s+/); // Split by spaces and remove empty entries
+//   const totalWords = wordsArray.filter(word => word.length > 0).length; // Count valid words
+//   totalCounterEl.innerText = totalWords;
+//   const maxWords = 50;
+//   remainingCounterEl.innerText = maxWords - totalWords;
+// }
+
